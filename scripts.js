29d8265474fe96d6182226ec5f495762659de7fb
helloAmulya <internatomix@gmail.com>
2024-12-25 22:03:44 +0530

    new changes

--- scripts.js
+++ scripts.js
@@ -1,28 +1,28 @@
 document.getElementById('blogForm').addEventListener('submit', async function (e) {
-    e.preventDefault();
-  
-    const title = document.getElementById('title').value;
-    const content = document.getElementById('content').value;
-    const author = document.getElementById('author').value;
-  
-    try {
-      const response = await fetch('https://blog-backend-two-lyart.vercel.app/', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ title, content, author }),
-      });
-  
-      if (response.ok) {
-        alert('Blog posted successfully!');
-        document.getElementById('blogForm').reset();
-      } else {
-        alert('Error posting blog. Please try again.');
-      }
-    } catch (err) {
-      console.error('Error:', err);
-      alert('Failed to connect to the server.');
+  e.preventDefault();
+
+  const title = document.getElementById('title').value;
+  const content = document.getElementById('content').value;
+  const author = document.getElementById('author').value;
+
+  try {
+    const response = await fetch('https://rndblog.vercel.app/api/blogs', {
+
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ title, content, author }),
+    });
+
+    if (response.ok) {
+      alert('Blog posted successfully!');
+      document.getElementById('blogForm').reset();
+    } else {
+      alert('Error posting blog. Please try again.');
     }
-  });
-  
+  } catch (err) {
+    console.error('Error:', err);
+    alert('Failed to connect to the server.');
+  }
+});
